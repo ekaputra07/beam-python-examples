fa4b5a4cafbb44b78530eb5bf5dedb03a612a3b5
Eka Putra <eka@insight.co>
2020-08-15 14:05:37 +1000

    Added simple workcount pipeline

---
+++ wordcount/Readme.md
@@ -0,0 +1,30 @@
+# Word Count
+This pipeline will count will count the number of occurrences of each word in a given input text.
+
+**Install Apache Beam Python SDK:**
+```
+pip install apache-beam
+
+# or use command below if you want to run it on GCP (Cloud Dataflow)
+pip install apache-beam[gcp]
+```
+
+**Run using Direct Runner (local)**
+```
+python wordcount.py
+```
+
+**Run on Cloud Dataflow**
+```
+python wordcount.py \
+--input=gs://dataflow-samples/shakespeare/kinglear.txt \
+--output=gs://my-bucket/wordcount/output \
+--runner=DataFlowRunner \
+--project=my-project \
+--staging_location=gs://my-bucket/wordcount/_staging \
+--temp_location=gs://my-bucket/wordcount/_temp \
+--job_name=word-count \
+--region=us-central1
+```
+
+Or use the `Makefile` as the shortcut command.
---
+++ wordcount/wordcount.py
@@ -0,0 +1,66 @@
+from __future__ import absolute_import
+
+import argparse
+import logging
+import re
+from past.builtins import unicode
+
+import apache_beam as beam
+from apache_beam.options.pipeline_options import PipelineOptions, SetupOptions
+
+
+class WordCountOptions(PipelineOptions):
+    @classmethod
+    def _add_argparse_args(cls, parser):
+        parser.add_argument(
+            '--input',
+            dest='input',
+            default='data/*',
+            help='Input file to process.'
+        )
+        parser.add_argument(
+            '--output',
+            dest='output',
+            default='output/wordcount',
+            help='Output file to write the results to.'
+        )
+
+
+class CountWords(beam.PTransform):
+    def expand(self, pcoll):
+        return (
+            pcoll
+            | 'Find words' >> beam.FlatMap(lambda line: re.findall(r'[a-zA-Z\']+', line))
+            | 'Pair words with 1' >> beam.Map(lambda w: (w.lower(), 1))
+            # can be replaced with: beam.combiners.Count.PerElement()
+            | 'Group by word and sum' >> beam.CombinePerKey(sum)
+        )
+
+
+class FormatAsText(beam.DoFn):
+    def process(self, element):
+        print(element)
+        word, count = element
+        yield '%s: %s' % (word, count)  # use yield instead of return
+
+
+def run(save_main_session=True):
+    """main entry point"""
+
+    pipeline_options = WordCountOptions()
+    pipeline_options.view_as(
+        SetupOptions).save_main_session = save_main_session
+
+    with beam.Pipeline(options=pipeline_options) as pipeline:
+        (
+            pipeline
+            | 'read lines' >> beam.io.ReadFromText(pipeline.options.input)
+            | CountWords()
+            | 'Format result' >> beam.ParDo(FormatAsText())
+            | 'Write results' >> beam.io.WriteToText(pipeline.options.output)
+        )
+
+
+if __name__ == '__main__':
+    logging.getLogger().setLevel(logging.INFO)
+    run()
